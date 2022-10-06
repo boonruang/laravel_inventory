a973005635da383e6c5a3b1b55a07d4721146ed2
boonruang <scubatoy@gmail.com>
2022-10-07 02:04:00 +0700

    27. Admin Logout Option

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -17,7 +17,7 @@
                         <img src="{{ asset('backend/assets/images/logo-sm.png') }}" alt="logo-sm-light" height="22">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('backend/assets/images/logo-light.png') }}" alt="logo-light" height="20">
+                        <img src="{{ asset('logo/logo-light.png') }}" alt="logo-light" height="20">
                     </span>
                 </a>
             </div>
@@ -50,7 +50,7 @@
             <div class="dropdown d-inline-block user-dropdown">
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user" src="{{ asset('backend/assets/images/users/avatar-1.jpg') }}"
+                    <img class="rounded-circle header-profile-user" src="{{ asset('backend/assets/images/users/avatar-7.jpg') }}"
                         alt="Header Avatar">
                     <span class="d-none d-xl-inline-block ms-1">Julia</span>
                     <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
@@ -62,7 +62,7 @@
                     <a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end mt-1">11</span><i class="ri-settings-2-line align-middle me-1"></i> Settings</a>
                     <a class="dropdown-item" href="#"><i class="ri-lock-unlock-line align-middle me-1"></i> Lock screen</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item text-danger" href="#"><i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
+                    <a class="dropdown-item text-danger" href="{{ route('admin.logout')}}"><i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoController;
+use App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(DemoController::class)->group(function() {
+    Route::get('/about','Index')->name('about.page')->middleware('check');
+    Route::get('/contact','ContactMethod')->name('contact.page');
+});
+
+// Admin All Route
+Route::controller(AdminController::class)->group(function() {
+    Route::get('/admin/logout','destroy')->name('admin.logout');
 });
 
 Route::get('/dashboard', function () {
