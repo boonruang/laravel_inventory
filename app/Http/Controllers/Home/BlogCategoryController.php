ff245917ff030ab0e44bce608c62c666f57436e2
boonruang <scubatoy@gmail.com>
2022-10-12 14:36:21 +0700

    68. Backend Blog Category Setup Part 3

--- app/Http/Controllers/Home/BlogCategoryController.php
+++ app/Http/Controllers/Home/BlogCategoryController.php
@@ -16,7 +16,7 @@ class BlogCategoryController extends Controller
 
     public function AddBlogCategory(){
         return view('admin.blog_category.blog_category_add');
-    }
+    } // End Metho
 
     public function StoreBlogCategory(Request $request){
         $request->validate([
@@ -35,5 +35,39 @@ class BlogCategoryController extends Controller
             'alert-type' => 'success'
         );        
         return redirect()->route('all.blog.category')->with($notification);
+    } // End Method
+
+    public function EditBlogCategory($id){
+        $blogcategory = BlogCategory::findOrFail($id);
+
+        return view('admin.blog_category.blog_category_edit',compact('blogcategory'));
+
+    } //End Method
+
+    public function UpdateBlogCategory(Request $request,$id){
+
+        BlogCategory::findOrFail($id)->update([
+            'blog_category' => $request->blog_category,
+        ]);
+
+        $notification = array(
+            'message' => 'Blog Category Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.blog.category')->with($notification);
     }
+
+    public function DeleteBlogCategory($id){
+
+        BlogCategory::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Blog Category Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }
+
 }
